2adb27f755034545fef757b9a19ed8486194eb4f
Tuomas Jormola <tj@suula.io>
2018-03-09 10:45:16 +0200

    Provide DefaultKafkaHeaderMapper bean with org.springframework.http added to the list of trusted packages

--- integration-tests/src/main/kotlin/demo/test/ReactiveIntegrationTestApplication.kt
+++ integration-tests/src/main/kotlin/demo/test/ReactiveIntegrationTestApplication.kt
@@ -7,7 +7,11 @@ import demo.source.HttpSourceConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.cloud.stream.test.binder.TestSupportBinderAutoConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
+import org.springframework.kafka.support.DefaultKafkaHeaderMapper
+import org.springframework.kafka.support.KafkaHeaderMapper
 
 @SpringBootApplication(exclude = arrayOf(TestSupportBinderAutoConfiguration::class))
 @Import(value = arrayOf(HttpSourceConfiguration::class, ReactiveProcessorConfiguration::class))
@@ -16,4 +20,15 @@ class ReactiveIntegrationTestApplication
 fun main(args: Array<String>) {
     SpringApplicationBuilder().sources(HttpSourceApplication::class.java, ReactiveProcessorApplication::class.java)
             .run(*args)
+}
+
+@Configuration
+class ReactiveIntegrationTestConfiguration {
+
+    @Bean
+    fun customKafkaHeaderMapper(): KafkaHeaderMapper {
+        val headerMapper = DefaultKafkaHeaderMapper()
+        headerMapper.addTrustedPackages("org.springframework.http")
+        return headerMapper
+    }
 }
